ff75af7984f6dec9ab01d3e8879eeea608a49f1a
hellofromtonya <hellofromtonya@knowthecode.io>
2018-02-08 11:59:48 -0600

    Improved tests bootstraps.
    1. Corrected/added @group
    2. Switched die() to trigger_error() for consistency.
    3. Changes BEANS_ROOT_DIR to BEANS_THEME_DIR for consistency.
    4. Abstracted the task of finding the WP tests dir into a function in integration bootstrap to improve readability.
    5. Stripped off the trailing directory separator, if it exists.

--- tests/phpunit/integration/bootstrap.php
+++ tests/phpunit/integration/bootstrap.php
@@ -7,13 +7,14 @@
  * @link        http://www.getbeans.io
  * @license     GNU-2.0+
  *
- * @group       integrationtests
+ * @group       integration-tests
  */
 
 if ( ! file_exists( '../../../wp-content' ) ) {
-	return;
+	trigger_error( 'Unable to run the integration tests, as the wp-content folder does not exist.', E_USER_ERROR );  // @codingStandardsIgnoreLine.
 }
 
+define( 'BEANS_TESTS_DIR', __DIR__ );
 define( 'BEANS_THEME_DIR', dirname( dirname( dirname( __DIR__ ) ) ) );
 define( 'WP_CONTENT_DIR', dirname( dirname( dirname( getcwd() ) ) ) . '/wp-content/' ); // @codingStandardsIgnoreLine.
 
@@ -21,27 +22,44 @@ if ( defined( 'WP_CONTENT_DIR' ) && ! defined( 'WP_PLUGIN_DIR' ) ) {
 	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . 'plugins/' ); // @codingStandardsIgnoreLine.
 }
 
-$beans_tests_dir = getenv( 'WP_TESTS_DIR' );
+/**
+ * Get the WordPress' tests suite directory.
+ *
+ * @since 1.5.0
+ *
+ * @return string
+ */
+function beans_get_wp_tests_dir() {
+	$tests_dir = getenv( 'WP_TESTS_DIR' );
 
-// Travis CI & Vagrant SSH tests directory.
-if ( empty( $beans_tests_dir ) ) {
-	$beans_tests_dir = '/tmp/wordpress-tests';
+	// Travis CI & Vagrant SSH tests directory.
+	if ( empty( $tests_dir ) ) {
+		$tests_dir = '/tmp/wordpress-tests';
+	}
+
+	// If the tests' includes directory does not exist, try a relative path to Core tests directory.
+	if ( ! file_exists( $tests_dir . '/includes/' ) ) {
+		$tests_dir = '../../../../tests/phpunit';
+	}
+
+	// Check it again. If it doesn't exist, stop here and post a message as to why we stopped.
+	if ( ! file_exists( $tests_dir . '/includes/' ) ) {
+		trigger_error( 'Unable to run the integration tests, as the WordPress test suite could not be located.', E_USER_ERROR );  // @codingStandardsIgnoreLine.
+	}
+
+	// Strip off the trailing directory separator, if it exists.
+	return rtrim( $tests_dir, DIRECTORY_SEPARATOR );
 }
 
-// Relative path to Core tests directory.
-if ( ! file_exists( $beans_tests_dir . '/includes/' ) ) {
-	$beans_tests_dir = '../../../../tests/phpunit';
-}
-
-if ( ! file_exists( $beans_tests_dir . '/includes/' ) ) {
-	trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR ); // @codingStandardsIgnoreLine.
-}
+// Find the WP tests suite directory.
+$beans_tests_dir = beans_get_wp_tests_dir();
 
 // Give access to tests_add_filter() function.
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+require_once $beans_tests_dir . '/includes/functions.php';
 
 /**
- * Loads theme.
+ * Register with "setup_theme" in the WP tests suite to set the themes directory
+ * and load the Beans framework.
  */
 tests_add_filter( 'setup_theme', function() {
 	register_theme_directory( WP_CONTENT_DIR . '/themes' );
@@ -49,4 +67,4 @@ tests_add_filter( 'setup_theme', function() {
 } );
 
 // Start up the WP testing environment.
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require $beans_tests_dir . '/includes/bootstrap.php';
