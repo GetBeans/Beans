b559a6367fbd265c9efb987ca281e96b57de1813
Christoph Herr <info@christophherr.com>
2018-03-06 19:32:16 -0500

    Refactored setUp() methods

--- tests/phpunit/unit/api/image/includes/class-image-test-case.php
+++ tests/phpunit/unit/api/image/includes/class-image-test-case.php
@@ -91,6 +91,7 @@ abstract class Image_Test_Case extends Test_Case {
 		parent::setUp();
 
 		require_once BEANS_TESTS_LIB_DIR . 'api/image/functions.php';
+		require_once BEANS_TESTS_LIB_DIR . 'api/image/class-beans-image-editor.php';
 
 		$this->load_original_functions( array(
 			'api/utilities/functions.php',
@@ -99,6 +100,9 @@ abstract class Image_Test_Case extends Test_Case {
 		$this->set_up_virtual_filesystem();
 		$this->images_dir = vfsStream::url( 'uploads/beans/images' );
 		$this->images_url = 'http:://example.com/uploads/beans/images/';
+
+		Monkey\Functions\when( 'beans_url_to_path' )->returnArg();
+		Monkey\Functions\when( 'beans_path_to_url' )->returnArg();
 
 		Monkey\Functions\expect( 'wp_upload_dir' )->andReturn( array(
 			'path'    => '',
@@ -109,12 +113,11 @@ abstract class Image_Test_Case extends Test_Case {
 			'error'   => false,
 		) );
 
-		Monkey\Functions\expect( 'site_url' )->andReturn( 'http:://example.com' );
-
 		$this->images = array(
 			$this->images_dir . '/image1.jpg' => static::$fixtures_dir . '/image1.jpg',
 			$this->images_dir . '/image2.jpg' => static::$fixtures_dir . '/image2.jpg',
 		);
+
 	}
 
 	/**
