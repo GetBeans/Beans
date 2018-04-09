3c8fdf2e1ef9e2e421312e6dfaafe0e188b21c3d
Caspar Green <cg@caspar.green>
2018-04-09 15:44:21 -0400

    Improve formatting of script view html

--- lib/api/post-meta/views/script.php
+++ lib/api/post-meta/views/script.php
@@ -10,26 +10,26 @@
 
 ?>
 
-    <script type="text/javascript">
-      ( function( $ ) {
-        $( document ).ready( function() {
-          $( '#page_template' ).data( 'beans-pre', $( '#page_template' ).val() );
-          $( '#page_template' ).change( function() {
-            var save = $( '#save-action #save-post' ),
-              meta = JSON.parse( '<?php echo wp_json_encode( $_beans_post_meta_conditions ); ?>' );
+<script type="text/javascript">
+	(function ($) {
+		$(document).ready(function () {
+			$('#page_template').data('beans-pre', $('#page_template').val());
+			$('#page_template').change(function () {
+				var save = $('#save-action #save-post'),
+					meta = JSON.parse('<?php echo wp_json_encode( $_beans_post_meta_conditions ); ?>');
 
-            if ( -1 === $.inArray( $( this ).val(), meta ) && -1 === $.inArray( $( this ).data( 'beans-pre' ), meta ) ) {
-              return;
-            }
+				if (-1 === $.inArray($(this).val(), meta) && -1 === $.inArray($(this).data('beans-pre'), meta)) {
+					return;
+				}
 
-            if ( save.length === 0 ) {
-              save = $( '#publishing-action #publish' );
-            }
+				if (save.length === 0) {
+					save = $('#publishing-action #publish');
+				}
 
-            $( this ).data( 'beans-pre', $( this ).val() );
-            save.trigger( 'click' );
-            $( '#wpbody-content' ).fadeOut();
-          } );
-        } );
-      } )( jQuery );
-    </script>
+				$(this).data('beans-pre', $(this).val());
+				save.trigger('click');
+				$('#wpbody-content').fadeOut();
+			});
+		});
+	})(jQuery);
+</script>
